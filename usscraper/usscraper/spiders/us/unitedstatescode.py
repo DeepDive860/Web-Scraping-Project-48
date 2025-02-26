5e3366eca93aa0f92d5153996b6caca9eb243385
DeepDive111-pixel <stinsonbolor@gmail.com>
2025-02-27 06:38:11 +0800

    Add new spider and JSON file

---
+++ usscraper/usscraper/spiders/us/unitedstatescode.py
@@ -0,0 +1,134 @@
+import pymysql.cursors
+import scrapy
+from scrapy.http import HtmlResponse
+from scrapy_playwright.page import PageMethod
+import pymysql
+
+
+class UsCode(scrapy.Spider):
+
+    name = "uscode"
+    allowed_domains = ["govinfo.gov"]
+    start_urls = ["https://www.govinfo.gov/app/search/%7B%22historical%22%3Atrue%2C%22offset%22%3A0%2C%22query%22%3A%22collection%3A(USCODE)%20AND%20publishdate%3Arange(%2C2025-02-21)%20%22%2C%22pageSize%22%3A100%7D"]
+
+    custom_settings = {
+        "PLAYWRIGHT_LAUNCH_OPTIONS": {"headless": False},
+        "PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT": 180000
+
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        try:
+            self.connection = pymysql.connect(
+                host="localhost",
+                user="root",
+                password="HAHAHA",
+                database="usgovdata",
+                charset="utf8mb4",
+                cursorclass=pymysql.cursors.DictCursor
+            )
+
+            self.cursor = self.connection.cursor()
+
+            
+        except Exception as e:
+            self.logger.warning(f"Connection failed {e}")
+
+
+
+    def start_requests(self):
+
+        for url in self.start_urls:
+
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse,
+                meta={
+                    "playwright": True,
+                    "playwright_include_page": True,
+                    "playwright_page_methods": [
+                        PageMethod("wait_for_selector", "li.results-row", timeout=180000, state="visible")
+                    ]
+                }
+            )
+
+    async def parse(self, response):
+        
+        page = response.meta["playwright_page"]
+        try:
+            items = response.css("li.results-row")
+
+            for item in items:
+                
+                header_name = item.css("h4.result-title p::text").get()
+                source_link = response.urljoin(item.css("h4.result-title a::attr(href)").get())
+                source_pdf = response.urljoin(item.css("a.btn-details[href$='.pdf']::attr(href)").get())
+
+                yield {
+                    "Header_Name": header_name,
+                    "Source-Link": source_link,
+                    "Source_Pdf": source_pdf
+                }
+
+                self.save_to_database(header_name, source_link, source_pdf)
+
+            while True:
+
+                next_button = await page.query_selector("li.page-item:not(.disabled) >> text='Next'")
+
+                if not next_button:
+                    break
+
+                await next_button.wait_for_element_state("visible")
+                await next_button.click()
+
+                content = await page.content()
+
+                updated_content = HtmlResponse(
+                    url=response.url,
+                    body=content,
+                    encoding='utf-8',
+                    request=response.request
+                )
+
+                async for item in self.parse(updated_content):
+                    yield item
+
+        except Exception as e:
+            self.logger.warning(f"Playwright error {e}")
+
+    def save_to_database(self, header_name, source_link, source_pdf):
+
+        try:
+
+            sql = "INSERT INTO data (title, source_url, source_pdf)  VALUES (%s, %s, %s)"
+            values = (header_name, source_link, source_pdf)
+
+            self.cursor.execute(sql, values)
+            self.connection.commit()
+            self.logger.info("Inserted Succcesfully")
+
+        except Exception as e:
+
+            self.logger.error(f"Error inserting {e}")
+            self.connection.rollback()
+
+            
+
+
+
+
+
+
+        
+
+
+
+
+
+        
+
+        
+
